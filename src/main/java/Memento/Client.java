c3c32705f5e3edd050962596b275d953e5a0efea
zopfds <740779066@qq.com>
2018-06-09 15:15:57 +0800

    备忘录模式

--- src/main/java/Memento/Client.java
+++ src/main/java/Memento/Client.java
@@ -4,13 +4,10 @@ public class Client {
     public static void main(String[] args){
         Originator originator = new Originator();
         originator.setState("born");
-        Memento bornMemento = originator.createMemento();
         CareTaker careTaker = new CareTaker();
-        careTaker.saveMemento(bornMemento);
+        careTaker.saveMemento(originator.createMemento());
 
         originator.setState("grow");
-        Memento growMemento = originator.createMemento();
-
         originator.restoreMemento(careTaker.retrieveMemento());
 
         System.out.println("state = " + originator.getState());

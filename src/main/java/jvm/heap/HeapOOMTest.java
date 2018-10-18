323df2a022919408671ee0181ba04b1e273ea052
jianbo.pan <jianbo.pan@mljr.com>
2018-10-18 18:51:40 +0800

    自定义生产者消费者

--- src/main/java/jvm/heap/HeapOOMTest.java
+++ src/main/java/jvm/heap/HeapOOMTest.java
@@ -3,10 +3,18 @@ package jvm.heap;
 import java.util.ArrayList;
 import java.util.List;
 
-public class OOMTest {
+/**
+ * 堆上分配对象导致堆内存溢出
+ */
+public class HeapOOMTest{
     public static void main(String[] args){
         List<InnerObject> objects = new ArrayList<>();
         while(true){
+            try {
+                Thread.currentThread().sleep(10);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
             objects.add(new InnerObject());
         }
     }
---
+++ src/main/java/thread/lock/ProducerAndConsumer.java
@@ -0,0 +1,104 @@
+package thread.lock;
+
+import java.util.concurrent.ConcurrentLinkedDeque;
+
+/**
+ * 生产者和消费者
+ * 第一版，本质是生产者和消费者两者线程间的协调
+ *
+ * @author jianbo.pan@mljr.com
+ * @version ${VERSION}
+ * @create 2018/10/17
+ */
+public class ProducerAndConsumer {
+
+    private static ConcurrentLinkedDeque<String> queue = new ConcurrentLinkedDeque<String>();
+
+    private static int maxSize = 10;
+
+    private static int maxProducer = 20;
+
+    private static int currentProduce = 0;
+
+    private static Object fullSign = new Object();
+
+    private static Object emptySign = new Object();
+
+    /**
+     * 消费者
+     * @return
+     */
+    public static String consume(){
+        while(true) {
+            //若队列为空则等待
+            if (queue.size() == 0) {
+                synchronized (emptySign) {
+                    try {
+                        System.out.println("queue empty , consumer thread wait!");
+                        emptySign.wait();
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }else{
+                String elem = queue.removeLast();
+                System.out.println("consumer consume object:" + elem);
+                if(queue.size() != maxProducer){
+                    synchronized (fullSign){
+                        fullSign.notify();
+                    }
+                }
+            }
+        }
+    }
+
+
+    /**
+     * 生产者
+     */
+    public static void producer(){
+        while(true && currentProduce < maxProducer){
+            if(queue.size() == maxSize){
+                synchronized (fullSign){
+                    try {
+                        System.out.println("queue full , producer thread wait!");
+                        fullSign.wait();
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }else{
+                String elem = String.valueOf(++currentProduce);
+                queue.addFirst(elem);
+                System.out.println("producer produce object:" + elem);
+                if(queue.size() == 1){
+                    synchronized (emptySign){
+                        emptySign.notify();
+                    }
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args){
+        ProducerAndConsumer producerAndConsumer = new ProducerAndConsumer();
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                producerAndConsumer.consume();
+            }
+        }).start();
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                producerAndConsumer.producer();
+            }
+        }).start();
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                producerAndConsumer.consume();
+            }
+        }).start();
+    }
+}
